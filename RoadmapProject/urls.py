00e3b6d20e615a2e46f9c47f539742ddc33540e0
yujin113 <claire9585@sookmyung.ac.kr>
2021-07-11 23:55:50 +0900

    메뉴바, 하단 공통 부분 작성 & infoPage 상단 작성

--- RoadmapProject/urls.py
+++ RoadmapProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import info.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('info/', info.views.info, name="info"),
 ]
