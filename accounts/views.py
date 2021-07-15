ca602fedd6de830eb0651bb94d51b801b188f25c
Ellie <darkisunho@naver.com>
2021-07-16 00:07:24 +0900

    accounts add

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-import django.contrib import auth
+from django.contrib import auth
 
 # Create your views here.    
 def home(request):
-    return render(request, 'home.html')
-    
+    return render(request, 'accounts/home.html')
+
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
@@ -16,8 +16,8 @@ def signup(request):
                 user=User.objects.create_user(
                     request.POST['username'],password=request.POST['password1']
                     )
-                    auth.login(request,user)
-                    return redirect('home')
+                auth.login(request,user)
+                return redirect('home')
         else:
             return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
     else:
