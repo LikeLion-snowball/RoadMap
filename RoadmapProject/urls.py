e1f67f72e546cb98cc38261b457b84375df148fe
Ohyaelim <npr05080@gmail.com>
2021-07-15 14:48:34 +0900

    Q&A 앱 만들기

--- RoadmapProject/urls.py
+++ RoadmapProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import cal.views
+import qna.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', cal.views.cal, name="cal"),
+    path('qna/', qna.views.qna, name="qna"),
 ]
