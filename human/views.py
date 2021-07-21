5509b50a4c825e487f306d906dca02e37b54303f
wona <wonajojo@gmail.com>
2021-07-21 23:26:40 +0900

    크리에이트까지 만들었으나 어드민 안됨

--- human/views.py
+++ human/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Human
+from .forms import PostForm
+from django.utils import timezone
 # Create your views here.
 
 def human(request):
@@ -10,6 +12,28 @@ def dpage(request, human_id):
     human_dpage = get_object_or_404(Human, pk=human_id)
     return render(request, 'dpage.html', {'human': human_dpage})
 
+
+def dwrite(request):
+    return render(request, 'dwrite.html')
+
+def dpostcreate(request):
+    human = Human()
+    human.title = request.GET['title']
+    human.body = request.GET['body']
+    human.pub_date = timezone.datetime.now()
+    human.save()
+    return redirect('human')
+
+def dpostupdate(request):
+    if request.method =='POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.save()
+            return redirect('dpage')
+    else :
+        form=PostForm()
+        return render(request,'dnew.html',{'form':form})
 def dnew(request):
     full_text = request.GET['fulltext']
 
@@ -25,7 +49,4 @@ def dnew(request):
             # add to the dictionary
             word_dictionary[word] = 1
 
-    return render(request, 'new.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()} )
-
-def dwrite(request):
-    return render(request, 'dwrite.html')
+    return render(request, 'dnew.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()} )
