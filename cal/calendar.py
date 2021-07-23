fb45a8d4e8c3b246720a026d74ab13b8f56bc9fc
yujin113 <claire9585@sookmyung.ac.kr>
2021-07-23 21:32:49 +0900

    일정관리 css 수정

--- cal/calendar.py
+++ cal/calendar.py
@@ -36,4 +36,5 @@ class Calendar(HTMLCalendar):
 		cal += f'{self.formatweekheader()}\n'
 		for week in self.monthdays2calendar(self.year, self.month):
 			cal += f'{self.formatweek(week, events)}\n'
+		cal += f'</table>'
 		return cal
