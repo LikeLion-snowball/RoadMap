de53c9bcf16430ad2662f57c073b4eed56742860
yujin113 <claire9585@sookmyung.ac.kr>
2021-07-30 16:14:09 +0900

    댓글 완료

--- commentcrud/urls.py
+++ commentcrud/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('commentcreate/<int:user_id>/<int:post_id>', views.commentcreate,name='commentcreate'),
-    path('commentupdate/<int:post_id>/<int:comment_id>',views.commentupdate,name='commentupdate'),
-    path('commentdelete/<int:post_id>/<int:comment_id>',views.commentdelete,name='commentdelete'),
+    path('commentupdate/<int:post_id>/<int:comment_id>/<int:user_id>',views.commentupdate,name='commentupdate'),
+    path('commentdelete/<int:post_id>/<int:comment_id>/<int:user_id>',views.commentdelete,name='commentdelete'),
 ]
--- commentcrud/views.py
+++ commentcrud/views.py
@@ -4,40 +4,23 @@ from human.models import Humanlog
 from .models import Comment
 from accounts.models import CustomUser
 
-# Create your views here.
-# def commentcreate(request, user_id, post_id):
-#     user = get_object_or_404(CustomUser, pk=user_id)
-#     post = get_object_or_404(Humanlog, pk=post_id)
-#     if request.method=='POST':
-#         form = CommentForm(request.POST)
-#         if form.is_valid():
-#             comment = form.save(commit=False)
-#             comment.user = user
-#             comment.post = post
-#             comment.save()
-#             return redirect('dpage', post_id=post.pk)
-#         else:
-#             redirect('list')
-#     else:
-#         form = CommentForm()
-#         return render(request, 'dpage.html', {'form': form, 'post': post})
-
-def commentupdate(request, post_id, comment_id):
+def commentupdate(request, post_id, comment_id, user_id):
     comment = get_object_or_404(Comment, pk=comment_id)
     post = get_object_or_404(Humanlog, pk=post_id)
+    user = get_object_or_404(CustomUser, pk=user_id)
     if request.method=='POST':
         form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.save()
-            return redirect('dpage', post_id=comment.post.pk)
-
+            return redirect('dpage', post_id=comment.post.pk, user_id=user.pk)
     else:
         form = CommentForm(instance=comment)
-        return render(request, 'dpage.html', {'form_comment': form, 'post': comment.post})
+        return render(request, 'dpage.html', {'form_comment': form, 'post': comment.post, 'commentId': comment_id})
 
-def commentdelete(request, post_id, comment_id):
+def commentdelete(request, post_id, comment_id, user_id):
     comment = get_object_or_404(Comment, pk=comment_id)
     post = get_object_or_404(Humanlog, pk=post_id)
+    user = get_object_or_404(CustomUser, pk=user_id)
     comment.delete()
-    return redirect('dpage', post_id=post.pk)     
+    return redirect('dpage', post_id=post.pk, user_id=user.pk)     
