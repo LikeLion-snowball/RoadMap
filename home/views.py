5a7fc693c153ed2e0b8acf0a5ecedeebe3ff8c1d
yujin113 <claire9585@sookmyung.ac.kr>
2021-08-12 01:24:43 +0900

    홈 css

--- home/views.py
+++ home/views.py
@@ -2,10 +2,11 @@ from info.models import Recruit
 from django.shortcuts import render,get_object_or_404
 from qna.models import Qna
 from human.models import Humanlog
+from datetime import date
 
 # Create your views here.
 def home(request):
     Qnadts = Qna.objects.order_by('-like_count')[:3]
     Humandts = Humanlog.objects.order_by('-like_count')[:3]
-    Recruitdts = Recruit.objects.order_by('-scrap_count')[:4]
-    return render(request, 'home.html', {'Qnadts' : Qnadts, 'Humandts' : Humandts, 'Recruitdts' : Recruitdts})
+    Recruitdts = Recruit.objects.all()[:4]
+    return render(request, 'home.html', {'Qnadts' : Qnadts, 'Humandts' : Humandts, 'Recruitdts' : Recruitdts, 'today': date.today()})
